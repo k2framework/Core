cfdf5aca61fbebd5a5499634005318fc42085689
Manuel Aguirre <programador.manuel@gmail.com>
2012-09-01 12:28:31 -0430

    documentando las clases del kernel

--- Kernel/AppContext.php
+++ Kernel/AppContext.php
@@ -5,24 +5,76 @@ namespace KumbiaPHP\Kernel;
 use KumbiaPHP\Kernel\Request;
 
 /**
- * Description of RouteContext
+ * Clase que contiene la info del contexto en el que se encuentra la aplicación
+ * actualmente.
  *
  * @author maguirre
  */
 class AppContext
 {
 
+    /**
+     * Url base del proyecto
+     * @var string 
+     */
     protected $baseUrl;
+
+    /**
+     * Ruta hacia el directorio app del proyecto
+     * @var string 
+     */
     protected $appPath;
+
+    /**
+     * Ruta hacia el directorio modules del poryecto
+     * @var string 
+     */
     protected $modulesPath;
-    protected $routes;
+
+    /**
+     * Contiene la url actual de la petición
+     * @var string 
+     */
     protected $currentUrl;
+
+    /**
+     * Areglo con las rutas y prefijos de los modulos del proyecto
+     * @var array 
+     */
     protected $modules;
+
+    /**
+     * Areglo con las rutas y nombres de los namespaces del proyecto
+     * @var array 
+     */
     protected $namespaces;
+
+    /**
+     * Contiene el prefijo actual que representa a un modulo en el proyecto
+     * @var string 
+     */
     protected $currentModule;
+
+    /**
+     * Contiene el nombre del controlador actual ejecutandose en el proyecto
+     * @var string 
+     */
     protected $currentController;
+
+    /**
+     * indica si el proyecto está en producción ó no.
+     * @var boolean 
+     */
     protected $inProduction;
 
+    /**
+     * Constructor de la clase
+     * @param Request $request
+     * @param type $inProduction
+     * @param type $appPath
+     * @param type $modules
+     * @param type $namespaces 
+     */
     public function __construct(Request $request, $inProduction, $appPath, $modules, $namespaces)
     {
         $this->baseUrl = $request->getBaseUrl();
@@ -34,36 +86,66 @@ class AppContext
         $this->namespaces = $namespaces;
     }
 
+    /**
+     * Establece la nueva url cuando se hace un forward.
+     * @param Request $request 
+     */
     public function setRequest(Request $request)
     {
         $this->currentUrl = $request->getRequestUrl();
     }
 
+    /**
+     * Devuelve la url base del proyecto
+     * @return string 
+     */
     public function getBaseUrl()
     {
         return $this->baseUrl;
     }
 
+    /**
+     * Devuelve la ruta hacia la carpeta app
+     * @return string 
+     */
     public function getAppPath()
     {
         return $this->appPath;
     }
 
+    /**
+     * devuelve la url actual de la petición
+     * @return string 
+     */
     public function getCurrentUrl()
     {
         return $this->currentUrl;
     }
 
+    /**
+     * Devuelve la ruta hacia la carpeta de los modulos de la app
+     * @return string 
+     */
     public function getModulesPath()
     {
         return $this->modulesPath;
     }
 
+    /**
+     * devuelve los namespaces registrados en el proyecto
+     * @return array 
+     */
     public function getNamespaces()
     {
         return $this->namespaces;
     }
 
+    /**
+     * devuelve los modulos registrados en el proyecto
+     * @param string $route si se suministra un prefijo, devuelve solo
+     * el valor de la ruta para ese prefijo.
+     * @return array|string|NULL 
+     */
     public function getModules($route = NULL)
     {
         if ($route) {
@@ -73,29 +155,50 @@ class AppContext
         }
     }
 
+    /**
+     * Devuelve el prefijo actual del modulo que se está ejecutando
+     * @return string 
+     */
     public function getCurrentModule()
     {
         return $this->currentModule;
     }
 
+    /**
+     * Establece el modulo actual en ejecucion
+     * @param string $currentModule 
+     */
     public function setCurrentModule($currentModule)
     {
         $this->currentModule = $currentModule;
     }
 
+    /**
+     * Devuelve el nombre del controlador actual en ejecución
+     * @return string 
+     */
     public function getCurrentController()
     {
         return $this->currentController;
     }
 
+    /**
+     * Establece el nombre del controlador actual en ejecución
+     * @param string $currentController 
+     */
     public function setCurrentController($currentController)
     {
         $this->currentController = $currentController;
     }
 
+    /**
+     * devuelve TRUE si la app se encuentra en producción.
+     * @return boolean 
+     */
     public function InProduction()
     {
         return $this->inProduction;
     }
+
 }
 
--- Kernel/Request.php
+++ Kernel/Request.php
@@ -6,7 +6,7 @@ use KumbiaPHP\Kernel\Session\SessionInterface;
 use KumbiaPHP\Kernel\Parameters;
 
 /**
- * Esta clase representa una petici�n HTTP.
+ * Esta clase representa una petición HTTP.
  *
  * @author manuel
  */
@@ -44,7 +44,7 @@ class Request
     public $files;
 
     /**
-     * Contiene la Informaci�n de la Sesi�n
+     * Contiene la Información de la Sesi�n
      * @var SessionInterface 
      */
     protected $session;
@@ -54,8 +54,21 @@ class Request
      * @var string 
      */
     private $baseUrl;
+
+    /**
+     * Contenido del Request Body de la petición
+     * @var string 
+     */
     protected $content = FALSE;
 
+    /**
+     * Constructor de la clase. 
+     * Rellena los parametros de la clase con la info de la petición
+     * los valores de las variables globales $_SERVER, $_POST, $_GET , $_COOKIE
+     * $_FILES pasan a estár contenidos en atributos de la clase con el fin
+     * de ofrecer una arquitectura de comunicacion con estos valores orientada 
+     * a objetos. 
+     */
     public function __construct()
     {
         $this->server = new Parameters($_SERVER);
@@ -64,16 +77,33 @@ class Request
         $this->cookies = new Parameters($_COOKIE);
         $this->files = new Parameters($_FILES);
 
+        //este fix es para permitir tener en el request los valores para peticiones
+        //PUT y DELETE, ya que php no ofrece una forma facil de obtenerlos
+        //actualmente.
         if (0 === strpos($this->server->get('CONTENT_TYPE'), 'application/x-www-form-urlencoded')
-                && in_array($this->server->get('REQUEST_METHOD'), array('PUT', 'DELETE'))
+                && in_array($this->getMethod(), array('PUT', 'DELETE'))
         ) {
             parse_str($this->getContent(), $data);
             $this->request = new Parameters($data);
         } elseif (0 === strpos($this->server->get('CONTENT_TYPE'), 'application/json')) {
+            //si los datos de la petición se envian en formato JSON
+            //los convertimos en una arreglo.
             $this->request = new Parameters((array) json_decode($this->getContent(), TRUE));
         }
     }
 
+    /**
+     * Devuelve el valor para un indice de las variables globales de la petición
+     * 
+     * Primero busca en request, sino lo encuentra busca en query y si no existe
+     * lo busca en las cookies.
+     * 
+     * De no exitir en ninguna de las variables devuelve el valor por defecto.
+     * 
+     * @param string $key
+     * @param mixed $default
+     * @return mixed 
+     */
     public function get($key, $default = NULL)
     {
         //busca en request, si no existe busca en query sino existe busca en 
@@ -82,7 +112,7 @@ class Request
     }
 
     /**
-     *
+     * Devuelve la instancia del manejador de sesiones.
      * @return SessionInterface 
      */
     public function getSession()
@@ -91,7 +121,7 @@ class Request
     }
 
     /**
-     * @Service(session)
+     * Estabelce la instancia del manejador de sesión
      * @param SessionInterface $session 
      */
     public function setSession(SessionInterface $session)
@@ -99,26 +129,47 @@ class Request
         $this->session = $session;
     }
 
+    /**
+     * Devuelve el metodo de la petición
+     * @return string 
+     */
     public function getMethod()
     {
         return $this->server->get('REQUEST_METHOD', 'GET');
     }
 
+    /**
+     * Devuelve la IP del cliente
+     * @return string 
+     */
     public function getClientIp()
     {
         return $this->server->get('REMOTE_ADDR');
     }
 
+    /**
+     * Devuelve TRUE si la petición es Ajax
+     * @return boolean 
+     */
     public function isAjax()
     {
         return $this->server->get('HTTP_X_REQUESTED_WITH') === 'XMLHttpRequest';
     }
 
+    /**
+     * Devuelve TRUE si el metodo de la petición es el pasado por parametro
+     * @param string $method
+     * @return boolean 
+     */
     public function isMethod($method)
     {
         return $this->getMethod() === $method;
     }
 
+    /**
+     * Devuelve el url base del proyecto
+     * @return string 
+     */
     public function getBaseUrl()
     {
         if (!$this->baseUrl) {
@@ -127,11 +178,19 @@ class Request
         return $this->baseUrl;
     }
 
+    /**
+     * Devuelve la url de la petición actual
+     * @return type 
+     */
     public function getRequestUrl()
     {
         return $this->query->get('_url', '/');
     }
 
+    /**
+     * Devuelve el Cuerpo de la petición
+     * @return string 
+     */
     public function getContent()
     {
         if (FALSE === $this->content) {
@@ -140,6 +199,10 @@ class Request
         return $this->content;
     }
 
+    /**
+     * Crea la url base de la petición.
+     * @return string 
+     */
     private function createBaseUrl()
     {
         //return dirname($this->server->get('SCRIPT_NAME')) . '/';
