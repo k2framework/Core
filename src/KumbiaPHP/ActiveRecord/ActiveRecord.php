4c2e9bd5eb30abcd079c3c3e4c6980228deccc52
Manuel Aguirre <programador.manuel@gmail.com>
2012-09-01 23:04:09 -0430

    estableciendo el validador a usar por el active record

--- src/KumbiaPHP/ActiveRecord/ActiveRecord.php
+++ src/KumbiaPHP/ActiveRecord/ActiveRecord.php
@@ -7,11 +7,14 @@ use ActiveRecord\Config\Config;
 use KumbiaPHP\ActiveRecord\Config\Reader;
 use KumbiaPHP\Validation\Validatable;
 use KumbiaPHP\Validation\ValidationBuilder;
+use KumbiaPHP\Validation\Validator;
 
 if (!Config::initialized()) {
     //si no está inicializada la configuración que usa el Active Record,
     //lo inicializamos.
     Reader::readDatabases();
+    //establecemos el validador a usar por el active record
+    ActiveRecord::setValidator(\KumbiaPHP\Kernel\Kernel::getContainer()->get('validator'));
 }
 
 /**
@@ -22,9 +25,25 @@ if (!Config::initialized()) {
 class ActiveRecord extends Model implements Validatable
 {
 
+    /**
+     * 
+     * @var Validator 
+     */
+    private static $validator;
+
+    public static function setValidator(Validator $validator)
+    {
+        self::$validator = $validator;
+    }
+
     public function buildValidations(ValidationBuilder $builder)
     {
         
+    }
+
+    protected function validate($update = FALSE)
+    {
+        return self::$validator->validate($this);
     }
 
 }
