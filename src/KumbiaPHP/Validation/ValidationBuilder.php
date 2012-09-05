7c51d81e46eb00a91da5d4c9eb6d41c7f8c70eeb
Manuel Aguirre <programador.manuel@gmail.com>
2012-09-05 13:14:16 -0430

    agregando mas metodos a la clase

--- src/KumbiaPHP/Validation/ValidationBuilder.php
+++ src/KumbiaPHP/Validation/ValidationBuilder.php
@@ -11,16 +11,28 @@ class ValidationBuilder
 
     protected $valitations = array();
 
-    public function notNull($field, array $params = NULL)
+    public function notNull($field, array $params = array())
     {
         $this->valitations['NotNull'][$field] = $params;
         return $this;
     }
 
-    public function int($field, array $params = NULL)
+    public function int($field, array $params = array())
     {
         $this->valitations['Integer'][$field] = $params;
         return $this;
+    }
+
+    public function has($type, $field)
+    {
+        return isset($this->valitations[$type]) && isset($this->valitations[$type][$field]);
+    }
+
+    public function remove($type, $field)
+    {
+        if ($this->has($type, $field)) {
+            unset($this->valitations[$type][$field]);
+        }
     }
 
     public function getValidations()
