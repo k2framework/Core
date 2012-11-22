95fe832b3b55707af088bc644c16df55251c1225
Manuel Aguirre <programador.manuel@gmail.com>
2012-11-22 12:34:38 -0430

    adelantando las funcionalidades en la clase Translator

--- src/KumbiaPHP/Translation/Translator.php
+++ src/KumbiaPHP/Translation/Translator.php
@@ -2,16 +2,28 @@
 
 namespace KumbiaPHP\Translation;
 
+use KumbiaPHP\Di\Container\ContainerInterface;
 use KumbiaPHP\Translation\TranslatorInterface;
 
 class Translator implements TranslatorInterface
 {
-    
-    protected $appContext;
+
+    /**
+     *
+     * @var ContainerInterface 
+     */
+    protected $container;
+
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+    }
 
     public function trans($text, array $params = array(), $locale = null)
     {
+        //obtenemos el locale actual si no se especifica
+        $locale || $locale = $this->container->get('request')->getLocale();
         return $text;
     }
-
+    
 }
