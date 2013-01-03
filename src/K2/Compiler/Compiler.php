459c951c5ca1a5d8f47d3c885f6abb0b9b5a2f7d
Manuel Aguirre <programador.manuel@gmail.com>
2013-01-02 21:12:34 -0430

    mejorando el compiler

--- src/K2/Compiler/Compiler.php
+++ src/K2/Compiler/Compiler.php
@@ -4,6 +4,7 @@ namespace K2\Compiler;
 
 use K2\Compiler\CompilerInterface;
 use K2\Compiler\CompilerException;
+use Composer\Autoload\ClassLoader;
 
 class Compiler implements CompilerInterface
 {
@@ -11,6 +12,11 @@ class Compiler implements CompilerInterface
     protected $filename;
     protected $code;
     protected $config;
+    /**
+     *
+     * @var ClassLoader
+     */
+    protected $loader;
 
     /**
      * Arreglo con las rutas donde se van a buscar las clases.
@@ -18,25 +24,19 @@ class Compiler implements CompilerInterface
      */
     private $directories = array();
 
-    function __construct($filename)
+    function __construct(ClassLoader $loader, $filename)
     {
         if (!is_dir($dir = dirname($filename))) {
             throw new CompilerException("El directorio \"$dir\" No existe");
         }
         $this->filename = $filename;
+        
+        $this->loader = $loader;
 
         $this->config = parse_ini_file('compiler.ini', TRUE);
         $this->code = "<?php\n";
         var_dump($this->config);
         var_dump('Clases Compiladas:');
-    }
-
-    /**
-     * registra rutas donde se buscarán clases.
-     */
-    public function registerDirectories($directories)
-    {
-        $this->directories = $directories;
     }
 
     public function add($filename)
@@ -51,32 +51,11 @@ class Compiler implements CompilerInterface
         $this->code .= PHP_EOL . file_get_contents($filename);
     }
 
-    protected function getContent($className)
-    {
-        $original = $className;
-        $className = ltrim($className, '\\');
-        $fileName = '';
-        $namespace = '';
-        if ($lastNsPos = strripos($className, '\\')) {
-            $namespace = substr($className, 0, $lastNsPos);
-            $className = substr($className, $lastNsPos + 1);
-            $fileName = str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
-        }
-        $fileName .= str_replace('_', DIRECTORY_SEPARATOR, $className) . '.php';
-
-        foreach ($this->directories as $folder) {
-            if (is_file($file = $folder . DIRECTORY_SEPARATOR . $fileName)) {
-                var_dump($original);
-                $this->add($file);
-                return;
-            }
-        }
-    }
-
     public function compile()
     {
         foreach ($this->includedClasess() as $class) {
-            $this->getContent($class);
+            var_dump($this->loader->findFile($class));
+            $this->add($this->loader->findFile($class));
         }
 
         $compiled = str_replace("\n<?php", '', $this->code);
