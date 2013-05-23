3194c45039e73f49c580d889da1746abbc20b1d3
Manuel Aguirre <programador.manuel@gmail.com>
2013-05-23 11:46:56 -0430

    quitando uso del UserInterface

--- src/K2/Security/Auth/Token/ActiveRecord.php
+++ src/K2/Security/Auth/Token/ActiveRecord.php
@@ -2,11 +2,10 @@
 
 namespace K2\Security\Auth\Token;
 
-use K2\Security\Auth\User\UserInterface;
 use K2\Security\Auth\Token\TokenInterface;
 use K2\Security\Auth\Token\AbstractToken;
 
 class ActiveRecord extends AbstractToken implements TokenInterface
 {
-
+    
 }
