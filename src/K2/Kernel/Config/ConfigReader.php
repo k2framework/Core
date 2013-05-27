a7669c0234588bb661f7a2388645d75f8bd13dfa
manuel_j555 <programador.manuel@gmail.com>
2013-05-27 00:48:20 -0430

    actualizando el ConfigReader para hacerlo más simple

--- src/K2/Kernel/Config/ConfigReader.php
+++ src/K2/Kernel/Config/ConfigReader.php
@@ -2,34 +2,27 @@
 
 namespace K2\Kernel\Config;
 
+use K2\Kernel\App;
+
 class ConfigReader
 {
 
-    /**
-     *
-     * @var Collection
-     */
-    protected $config;
+    protected $file;
 
-//    private $sectionsValid = array('config', 'parameters');
-
-    public function __construct(AppContext $app)
+    public function __construct()
     {
-        $configFile = $app->getAppPath() . '/config/config.php';
-        if ($app->inProduction()) {
-            if (is_file($configFile)) {
-                $this->config = require $configFile;
-                return;
+        $this->file = APP_PATH . 'temp/cache/config.php';
+        if (PRODUCTION) {
+            if ($this->isCompiled()) {
+                App::addDefinitions(require $this->file);
             } else {
-                $this->config = $this->compile($app);
-                $config = PHP_EOL . PHP_EOL . 'return '
-                        . var_export($this->config, true);
-                file_put_contents($configFile, "<?php$config;");
+                $data['definitions'] = App::definitions();
+                $config = PHP_EOL . PHP_EOL . 'return ' . var_export($data, true);
+                file_put_contents($this->file, "<?php$config;");
             }
         } else {
-            $this->config = $this->compile($app);
-            if (is_writable($configFile)) {
-                unlink($configFile);
+            if (is_writable($this->file)) {
+                unlink($this->file);
             }
         }
     }
@@ -39,80 +32,14 @@ class ConfigReader
      * modulo en un solo esquema
      *  
      */
-    protected function compile(AppContext $app)
+    protected function compile()
     {
-        $parameters = array();
-        $services = array();
-
-        $dirs = array_merge($app->getModules(), array('app' => dirname($app->getAppPath())));
-
-        foreach ($dirs as $namespace => $dir) {
-            $configFile = rtrim($dir, '/') . '/' . $namespace . '/config/config.ini';
-            $servicesFile = rtrim($dir, '/') . '/' . $namespace . '/config/services.ini';
-
-            if (is_file($configFile)) {
-                foreach (parse_ini_file($configFile, true) as $sectionType => $values) {
-
-                    foreach ($values as $index => $v) {
-                        $parameters[$sectionType][$index] = $v;
-                    }
-                }
-            }
-            if (is_file($servicesFile)) {
-                foreach (parse_ini_file($servicesFile, TRUE) as $serviceName => $config) {
-                    if (isset($config['listen'])) {
-                        foreach ($config['listen'] as $method => $event) {
-                            $config['listen'][$method] = $event = explode(':', $event);
-                            isset($event[1]) || $config['listen'][$method][1] = 0;
-                        }
-                    }
-                    $services[$serviceName] = $config;
-                }
-            }
-        }
-
-        return $this->prepareAditionalConfig(array(
-                    'parameters' => $parameters,
-                    'services' => $services,
-                ));
+        
     }
 
-    public function getConfig()
+    public function isCompiled()
     {
-        return $this->config;
-    }
-
-    /**
-     * Añade configuraciones adicionales al arreglo de los servicios y patametros,
-     * como por ejemplo configurar el servicio que reescribe las url, para que sea
-     * el primero en ejecutarse, configurar el servicio de traducciones, etc.
-     * @param array $configs recibe las configuraciones de servicios y parametros
-     * @return array devuelve las configuraciones actualizadas
-     */
-    protected function prepareAditionalConfig($configs)
-    {
-        //si se usa el routes lo añadimos al container
-        if (isset($configs['parameters']['config']['routes'])) {
-            $router = substr($configs['parameters']['config']['routes'], 1);
-
-            //si es el router por defecto quien reescribirá las url
-            if ('router' === $router) {
-                //le añadimos un listener.
-                $configs['services']['router']
-                        ['listen']['rewrite'] = 'kumbia.request:1000'; //con priotidad 1000 para que sea el primero en ejecutarse.
-            }
-        }
-
-        //si se estan usando locales y ningun módulo a establecido una definición para
-        //el servicio translator, lo hacemos por acá.
-        if (isset($configs['parameters']['config']['locales'])
-                && !isset($configs['services']['translator'])) {
-            $configs['services']['translator'] = array(
-                'class' => 'K2\\Translation\\Translator',
-            );
-        }
-
-        return $configs;
+        return PRODUCTION && is_file($this->file);
     }
 
 }
