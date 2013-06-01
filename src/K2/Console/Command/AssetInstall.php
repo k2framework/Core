c8e7f0e3d5eeaa5543afa6d18ce6526c1fa5640a
manuel_j555 <programador.manuel@gmail.com>
2013-06-01 12:04:57 -0430

    usando el filesystem como un servicio

--- src/K2/Console/Command/AssetInstall.php
+++ src/K2/Console/Command/AssetInstall.php
@@ -6,7 +6,6 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use K2\Kernel\App;
-use Symfony\Component\Filesystem\Filesystem;
 
 /**
  * Description of AssetInstall
@@ -28,7 +27,7 @@ class AssetInstall extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $filesystem = new Filesystem();
+        $filesystem = App::get("filesystem");
 
         foreach (App::modules() as $name => $module) {
 
